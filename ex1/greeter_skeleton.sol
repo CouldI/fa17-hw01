b831b4190115fc7e660ff06dc802f7a00df7e6bd
CouldI <sym0222@naver.com>
2018-07-23 00:43:18 +0900

    ex1
    풀어보았슴.

--- ex1/greeter_skeleton.sol
+++ ex1/greeter_skeleton.sol
@@ -6,13 +6,17 @@ contract Greeter {
 	string greeting;
 
 	function Greeter(string _greeting) public {
-		/* Write one line of code for the contract to set our greeting */
+		greeting = _greeting; /* Write one line of code for the contract to set our greeting */
 	}
 
 	function greet() constant returns (string)  {
-		/* Write one line of code to allow the contract to return our greeting */
+		return greeting; /* Write one line of code to allow the contract to return our greeting */
 	}
 
-	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function () {
+		revert();
+	}/* Add a fallback function to prevent contract payability and non-existent function calls */
 	
 }
+
+/* remix ide에서 deploy 할때 받는값이 string이므로 ""를 빼먹으면 오류가 난다. */
