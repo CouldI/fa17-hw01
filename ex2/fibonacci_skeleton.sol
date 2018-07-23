93e6cab4b5e7d182467627a979c459be2a82d969
CouldI <sym0222@naver.com>
2018-07-23 20:39:31 +0900

    mission complete
    문제풀었당 ㅎ

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -3,9 +3,20 @@ pragma solidity ^0.4.16;
 
 contract Fibonacci {
 	function calculate(uint position) returns (uint result) {
+		if (position == 0){
+		    result = 0;
+		}
+		else if (position == 1){
+		    result = 1;
+		}
+		else {
+		   result = calculate(position - 1) + calculate(position - 2);
+		}
 		/* Carry out calculations to find the nth Fibonacci number */
 	}
 
-	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function () {
+	    revert();
+	}/* Add a fallback function to prevent contract payability and non-existent function calls */
 
 }
